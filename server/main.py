c3983f7518a0ca45ed4168e19c1283a4d63295eb
momosuke <rusk.ryoy4n@gmail.com>
2020-08-18 16:05:09 +0900

    fix: main.pyをflaskの様式に調整

--- server/main.py
+++ server/main.py
@@ -7,6 +7,7 @@
 #========================================================#
 
 import webapp2
+from flask import *
 import json
 
 import update_db
@@ -21,19 +22,60 @@ import getaddress
 import preparetx
 import submittx
 
-app = webapp2.WSGIApplication([
-	# 公開 API
-	('/api/v1/recentblkid', getrecentblkid.handler),
-	('/api/v1/block', getblock.handler),
-	('/api/v1/recenttxid', getrecenttxid.handler),
-	('/api/v1/transaction', gettransaction.handler),
-	('/api/v1/balance', getbalance.handler),
-	('/api/v1/millionaires', getmillionaires.handler),
-	('/api/v1/address', getaddress.handler),
-	('/api/v1/preparetx', preparetx.handler),
-	('/api/v1/submittx', submittx.handler),
+# app = webapp2.WSGIApplication([
+# 	# 公開 API
+# 	('/api/v1/recentblkid', getrecentblkid.handler),
+# 	('/api/v1/block', getblock.handler),
+# 	('/api/v1/recenttxid', getrecenttxid.handler),
+# 	('/api/v1/transaction', gettransaction.handler),
+# 	('/api/v1/balance', getbalance.handler),
+# 	('/api/v1/millionaires', getmillionaires.handler),
+# 	('/api/v1/address', getaddress.handler),
+# 	('/api/v1/preparetx', preparetx.handler),
+# 	('/api/v1/submittx', submittx.handler),
 
-	# 非公開 API
-	('/maintain/sendrawtransaction', sendrawtransaction.handler),
-	('/maintain/cron/update_db', update_db.handler)
-])
+# 	# 非公開 API
+# 	('/maintain/sendrawtransaction', sendrawtransaction.handler),
+# 	('/maintain/cron/update_db', update_db.handler)
+# ])
+
+app = Flask(__name__)
+
+@app.route("/api/v1/recentblkid")
+def recentblkid():
+    return getrecentblkid.handler().get(request)
+
+@app.route("/api/v1/block")
+def block():
+    return getblock.handler().get(request)
+
+@app.route("/api/v1/recenttxid")
+def recenttxid():
+    return getrecenttxid.handler().get(request)
+
+@app.route("/api/v1/transaction")
+def transaction():
+    return gettransaction.handler().get(request)
+
+@app.route("/api/v1/balance")
+def balance():
+    return getbalance.handler().get(request)
+
+@app.route("/api/v1/millionaires")
+def millionaires():
+    return getmillionaires.handler().get(request)
+
+@app.route("/api/v1/address")
+def address():
+    return getaddress.handler().get(request)
+
+@app.route("/api/v1/preparetx")
+def preparetransaction():
+    return preparetx.handler().get(request)
+
+@app.route("/api/v1/submittx")
+def submittransaction():
+    return submittx.handler().get(request)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
