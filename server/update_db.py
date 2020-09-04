e389d237ed189e86a2e3e1965affd9e7412bece0
ry0y4n <rusk.ryoy4n@gmail.com>
2020-09-04 16:25:17 +0900

    sync書き戻し(全完了)

--- server/update_db.py
+++ server/update_db.py
@@ -44,12 +44,11 @@ def sync( conn, coind_type, max_leng ):
 
 	# コインノードが保持しているブロックの高さ
 	cd_height = cd.run( 'getblockcount', [] )
-	print(cd_height)
 
-	db = conn.cursor()
 	# 開始時の DB 内ブロック高
-	db.execute( 'SELECT IFNULL(MAX(height)+1,0) FROM blockheader' )
-	start_block_height = db.fetchone()['IFNULL(MAX(height)+1,0)']
+	with conn.cursor() as c:
+		c.execute( 'SELECT IFNULL(MAX(height)+1,0) FROM blockheader' )
+		start_block_height = c.fetchone()['IFNULL(MAX(height)+1,0)']
 
 	# 開始時刻を記録
 	start_time = time.time()
@@ -68,252 +67,232 @@ def sync( conn, coind_type, max_leng ):
 			return
 
 		# 以降の同期作業はトランザクション内で行う
-		# 新規追加するブロックのデータを取得
-		# ブロックハッシュを取得
-		blockhash = cd.run( 'getblockhash', [ int(block_height) ] )
-		# ブロックデータを取得
-		cd_block = cd.run( 'getblock', [ blockhash ] )
-		# 縮小版の json データ作成
-		# - 信用できないデータは出力時に補完する
-		block_json_reduce = copy.deepcopy( cd_block )
-		block_json_reduce.pop( 'nextblockhash', None )
-		block_json_reduce.pop( 'previousblockhash', None )
-		block_json_reduce.pop( 'confirmations' )
-		# マイナーは一旦空にしておく
-		miners = None
-
-		# 所属するトランザクションのデータを順次追加
-		for txid in cd_block['tx']:
-			if ignore_txid( coind_type, txid ):
-				continue
-
-			# トランザクションデータを取得する
-			if coind_type == 'bitcoind':
-				# bitcoind の場合、重複 TXID があるので、第三引数にブロックハッシュを渡す
-				cd_transaction = cd.run( 'getrawtransaction', [ txid, 1, blockhash ] )
-			else:
-				# それ以外の場合、monacoind は第三引数を与えるとエラーになる
-				cd_transaction = cd.run( 'getrawtransaction', [ txid, 1 ] )
-
+		with conn.cursor() as c:
+			# 新規追加するブロックのデータを取得
+			# ブロックハッシュを取得
+			blockhash = cd.run( 'getblockhash', [ int(block_height) ] )
+			# ブロックデータを取得
+			cd_block = cd.run( 'getblock', [ blockhash ] )
 			# 縮小版の json データ作成
-			# - 信用できないデータは出力時に補完する。大きいデータは捨てる。
-			tx_json_reduce = copy.deepcopy( cd_transaction )
-			tx_json_reduce.pop( 'hex' )
-			tx_json_reduce.pop( 'confirmations' )
-			for e in tx_json_reduce['vin']:
-				if 'scriptSig' in e:
-					e['scriptSig'].pop( 'hex' )
-			for e in tx_json_reduce['vout']:
-				e['scriptPubKey'].pop( 'hex' )
+			# - 信用できないデータは出力時に補完する
+			block_json_reduce = copy.deepcopy( cd_block )
+			block_json_reduce.pop( 'nextblockhash', None )
+			block_json_reduce.pop( 'previousblockhash', None )
+			block_json_reduce.pop( 'confirmations' )
+			# マイナーは一旦空にしておく
+			miners = None
 
-			# BIP-30 違反検査は通常は対象とする
-			bip30_exception = False
-
-			# BIP-34 適用以前には 2 つだけ BIP-30 に違反するトランザクションが存在する
-			if coind_type == 'bitcoind':
-				if block_height == 91842 and blockhash == '00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec':
-					bip30_exception = True
-				elif block_height == 91880 and blockhash == '00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721':
-					bip30_exception = True
-
-			# BIP-30 違反検査は一旦強制的に無効化する
-			bip30_exception = True
-			# BIP-30 を厳密に守ろうとすると、バースデイパラドックスや鳩ノ巣原理によって
-			# 通常のトランザクションでの TXID 衝突まで考慮して、全検査を行う必要があるはずだが、
-			# bitcoin 開発陣は BIP-34 による coinbase トランザクションの対策だけで
-			# 十分に BIP-30 を守ることができると考えているらしく、BIP-34 施行以降は BIP-30 違反検査を省略している。
-			# また、BIP-34 施行以前の coinbase トランザクションとの衝突可能性についても考察されており、
-			# 次に問題となり得るのはブロック高 1,983,702 で、およそ 2045 年ごろと予測しているため、
-			# それまでは完全に BIP-30 違反検査を省略するよう、bitcoind が実装されている。
-			# bitcoind からフォークした monacoind も同様の実装となっており、こちらは
-			# BIP-34 施行以降であるため、施行以前の coinbase トランザクションとの衝突も気にする必要がなく、
-			# 完全に BIP-30 違反検査を省略する実装となっている。
-			# つまり、確率的にはいずれ TXID の衝突が発生するはずだが、現状の bitcoind, monacoind の実装では
-			# それを検知できずに承認してしまうという問題を抱えている。
-			# 現実問題として、実際に衝突が発生した時点で問題が発生するため、急遽 BIP が作られるだろうから、
-			# そのタイミングで VP-clerk も追従すればよいという方針で、とりあえず現状は、
-			# coind の承認したトランザクションは全て受け入れるものとして実装を進める。
-
-			# BIP-30 違反検査
-			if not bip30_exception:
-				db.execute( 'SELECT * FROM transaction_link WHERE ISNULL(vin_height) AND ISNULL(vin_txid) AND ISNULL(vin_idx) AND vout_txid = %s', (txid,) )
-				if len( db.fetchall() ) != 0:
-					raise Exception( 'BIP-30 violation' )
-
-			# このトランザクションの合計出金額を計算する
-			total_output = 0
-			for vout in cd_transaction['vout']:
-				total_output += vout['value'] * SATOSHI_COIN
-			# トランザクションデータの作成
-			db = conn.cursor()
-			db.execute(
-				'INSERT INTO transaction VALUES ( %s, %s, %s, %s, %s, %s, %s, %s )',
-				(
-					txid,
-					block_height,
-					cd_block['hash'],
-					datetime.fromtimestamp( cd_transaction['time'] ),
-					len( cd_transaction['vin'] ),
-					len( cd_transaction['vout'] ),
-					total_output,
-					base64.b64encode( bz2.compress( json.dumps( tx_json_reduce ).encode() ) ).decode()
-				)
-			)
-			db.close()
-			conn.commit()
-
-			# トランザクションリンクの片割れを作成
-			destinations = []
-			for vout in cd_transaction['vout']:
-				# 宛先アドレスの組み立て
-				scriptPubKey = vout['scriptPubKey']
-				if 'addresses' in scriptPubKey:
-					vout_addr = ' '.join( scriptPubKey['addresses'] )
-
-					# 宛先全部のリストを構成
-					destinations.append( vout_addr )
-				else:
-					vout_addr = None
-
-				db = conn.cursor()
-				# リンクデータ作成
-				db.execute(
-					'INSERT INTO transaction_link VALUES ( %s, %s, %s, %s, %s, %s, %s, %s )',
-					(
-						None, # 受け取り TX の時に書くので UTXO として None を書く
-						None,
-						None,
-						block_height,
-						txid,
-						vout['n'],
-						vout_addr,
-						vout['value'] * SATOSHI_COIN
-					)
-				)
-				db.close()
-				conn.commit()
-
-			# コインベーストランザクションの場合はマイナーとして宛先を設定
-			if 'coinbase' in cd_transaction['vin'][0]:
-				miners = ' '.join( destinations )
-
-			# トランザクションリンクの対向側を設定
-			for idx in range( len( cd_transaction['vin'] ) ):
-				vin = cd_transaction['vin'][idx]
-				if 'txid' in vin:
-					db = conn.cursor()
-					db.execute(
-						'UPDATE transaction_link SET vin_height = %s, vin_txid = %s, vin_idx = %s WHERE ISNULL(vin_height) AND ISNULL(vin_txid) AND ISNULL(vin_idx) AND vout_txid = %s AND vout_idx = %s',
-						(
-							block_height,
-							txid,
-							idx,
-							vin['txid'],
-							vin['vout']
-						)
-					)
-					db.close()
-					conn.commit()
-
-					if db.rowcount != 1:
-						if db.rowcount == 0:
-							# CVE-2018-17144 の攻撃対象となったトランザクションの場合は重複利用を無視する
-							if not CVE_2018_17144( coind_type, txid ):
-								raise Exception( block_height, txid, idx, 'missing transaction_link' )
-						else:
-							raise Exception( block_height, txid, idx, 'BIP-30 violation' )
-
-			# トランザクションでの残高増減表を作成
-			alter_balance = {}
-			db = conn.cursor()
-			db.execute( 'SELECT * FROM transaction_link WHERE (vin_height = %s AND vin_txid = %s) OR (vout_height = %s AND vout_txid = %s)', (block_height, txid, block_height, txid) )
-			for e in db.fetchall():
-				addr = e['addresses']
-				# アドレスがない場合は無視
-				if addr is None:
+			# 所属するトランザクションのデータを順次追加
+			for txid in cd_block['tx']:
+				if ignore_txid( coind_type, txid ):
 					continue
 
-				# 該当アドレスの情報がなければ先に 0 クリア
-				if not addr in alter_balance:
-					alter_balance[addr] = 0
-
-				# どちらにヒットしたかによって増減表を更新する
-				if e['vin_height'] == block_height and e['vin_txid'] == txid:
-					alter_balance[addr] -= e['value']
-				elif e['vout_height'] == block_height and e['vout_txid'] == txid:
-					alter_balance[addr] += e['value']
+				# トランザクションデータを取得する
+				if coind_type == 'bitcoind':
+					# bitcoind の場合、重複 TXID があるので、第三引数にブロックハッシュを渡す
+					cd_transaction = cd.run( 'getrawtransaction', [ txid, 1, blockhash ] )
 				else:
-					raise Exception( 'unknown transaction_link' )
-			db.close()
-			conn.commit()
-			# レコードを追加する
-			for addr, gain in alter_balance.items():
-				# 直前までの残高と次のシリアルナンバーを取得する
-				balance = 0
-				serial = 0
-				db = conn.cursor()
-				db.execute( 'SELECT serial, balance FROM balance WHERE addresses = %s ORDER BY serial DESC LIMIT 1', (addr,) )
-				for e in db.fetchall():
-					balance = e['balance']
-					serial = e['serial'] + 1
+					# それ以外の場合、monacoind は第三引数を与えるとエラーになる
+					cd_transaction = cd.run( 'getrawtransaction', [ txid, 1 ] )
 
-				# 残高更新
-				balance += gain
+				# 縮小版の json データ作成
+				# - 信用できないデータは出力時に補完する。大きいデータは捨てる。
+				tx_json_reduce = copy.deepcopy( cd_transaction )
+				tx_json_reduce.pop( 'hex' )
+				tx_json_reduce.pop( 'confirmations' )
+				for e in tx_json_reduce['vin']:
+					if 'scriptSig' in e:
+						e['scriptSig'].pop( 'hex' )
+				for e in tx_json_reduce['vout']:
+					e['scriptPubKey'].pop( 'hex' )
 
-				# レコード追加
-				db.execute(
-					'INSERT INTO balance VALUES ( %s, %s, %s, %s, %s, %s, %s )',
+				# BIP-30 違反検査は通常は対象とする
+				bip30_exception = False
+
+				# BIP-34 適用以前には 2 つだけ BIP-30 に違反するトランザクションが存在する
+				if coind_type == 'bitcoind':
+					if block_height == 91842 and blockhash == '00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec':
+						bip30_exception = True
+					elif block_height == 91880 and blockhash == '00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721':
+						bip30_exception = True
+
+				# BIP-30 違反検査は一旦強制的に無効化する
+				bip30_exception = True
+				# BIP-30 を厳密に守ろうとすると、バースデイパラドックスや鳩ノ巣原理によって
+				# 通常のトランザクションでの TXID 衝突まで考慮して、全検査を行う必要があるはずだが、
+				# bitcoin 開発陣は BIP-34 による coinbase トランザクションの対策だけで
+				# 十分に BIP-30 を守ることができると考えているらしく、BIP-34 施行以降は BIP-30 違反検査を省略している。
+				# また、BIP-34 施行以前の coinbase トランザクションとの衝突可能性についても考察されており、
+				# 次に問題となり得るのはブロック高 1,983,702 で、およそ 2045 年ごろと予測しているため、
+				# それまでは完全に BIP-30 違反検査を省略するよう、bitcoind が実装されている。
+				# bitcoind からフォークした monacoind も同様の実装となっており、こちらは
+				# BIP-34 施行以降であるため、施行以前の coinbase トランザクションとの衝突も気にする必要がなく、
+				# 完全に BIP-30 違反検査を省略する実装となっている。
+				# つまり、確率的にはいずれ TXID の衝突が発生するはずだが、現状の bitcoind, monacoind の実装では
+				# それを検知できずに承認してしまうという問題を抱えている。
+				# 現実問題として、実際に衝突が発生した時点で問題が発生するため、急遽 BIP が作られるだろうから、
+				# そのタイミングで VP-clerk も追従すればよいという方針で、とりあえず現状は、
+				# coind の承認したトランザクションは全て受け入れるものとして実装を進める。
+
+				# BIP-30 違反検査
+				if not bip30_exception:
+					c.execute( 'SELECT * FROM transaction_link WHERE ISNULL(vin_height) AND ISNULL(vin_txid) AND ISNULL(vin_idx) AND vout_txid = %s', (txid,) )
+					if len( c.fetchall() ) != 0:
+						raise Exception( 'BIP-30 violation' )
+
+				# このトランザクションの合計出金額を計算する
+				total_output = 0
+				for vout in cd_transaction['vout']:
+					total_output += vout['value'] * SATOSHI_COIN
+				# トランザクションデータの作成
+				c.execute(
+					'INSERT INTO transaction VALUES ( %s, %s, %s, %s, %s, %s, %s, %s )',
 					(
-						addr,
-						block_height,
 						txid,
-						serial,
+						block_height,
+						cd_block['hash'],
 						datetime.fromtimestamp( cd_transaction['time'] ),
-						balance,
-						gain
+						len( cd_transaction['vin'] ),
+						len( cd_transaction['vout'] ),
+						total_output,
+						base64.b64encode( bz2.compress( json.dumps( tx_json_reduce ).encode() ) ).decode()
 					)
 				)
-				db.close()
-				conn.commit()
 
-				# 最終残高の更新
-				if serial == 0:
-					db = conn.cursor()
-					db.execute(
-						'INSERT INTO current_balance VALUES ( %s, %s )',
+				# トランザクションリンクの片割れを作成
+				destinations = []
+				for vout in cd_transaction['vout']:
+					# 宛先アドレスの組み立て
+					scriptPubKey = vout['scriptPubKey']
+					if 'addresses' in scriptPubKey:
+						vout_addr = ' '.join( scriptPubKey['addresses'] )
+
+						# 宛先全部のリストを構成
+						destinations.append( vout_addr )
+					else:
+						vout_addr = None
+
+					# リンクデータ作成
+					c.execute(
+						'INSERT INTO transaction_link VALUES ( %s, %s, %s, %s, %s, %s, %s, %s )',
+						(
+							None, # 受け取り TX の時に書くので UTXO として None を書く
+							None,
+							None,
+							block_height,
+							txid,
+							vout['n'],
+							vout_addr,
+							vout['value'] * SATOSHI_COIN
+						)
+					)
+					conn.commit()
+
+				# コインベーストランザクションの場合はマイナーとして宛先を設定
+				if 'coinbase' in cd_transaction['vin'][0]:
+					miners = ' '.join( destinations )
+
+				# トランザクションリンクの対向側を設定
+				for idx in range( len( cd_transaction['vin'] ) ):
+					vin = cd_transaction['vin'][idx]
+					if 'txid' in vin:
+						c.execute(
+							'UPDATE transaction_link SET vin_height = %s, vin_txid = %s, vin_idx = %s WHERE ISNULL(vin_height) AND ISNULL(vin_txid) AND ISNULL(vin_idx) AND vout_txid = %s AND vout_idx = %s',
+							(
+								block_height,
+								txid,
+								idx,
+								vin['txid'],
+								vin['vout']
+							)
+						)
+						conn.commit()
+
+						if c.rowcount != 1:
+							if c.rowcount == 0:
+								# CVE-2018-17144 の攻撃対象となったトランザクションの場合は重複利用を無視する
+								if not CVE_2018_17144( coind_type, txid ):
+									raise Exception( block_height, txid, idx, 'missing transaction_link' )
+							else:
+								raise Exception( block_height, txid, idx, 'BIP-30 violation' )
+
+				# トランザクションでの残高増減表を作成
+				alter_balance = {}
+				c.execute( 'SELECT * FROM transaction_link WHERE (vin_height = %s AND vin_txid = %s) OR (vout_height = %s AND vout_txid = %s)', (block_height, txid, block_height, txid) )
+				for e in c.fetchall():
+					addr = e['addresses']
+					# アドレスがない場合は無視
+					if addr is None:
+						continue
+
+					# 該当アドレスの情報がなければ先に 0 クリア
+					if not addr in alter_balance:
+						alter_balance[addr] = 0
+
+					# どちらにヒットしたかによって増減表を更新する
+					if e['vin_height'] == block_height and e['vin_txid'] == txid:
+						alter_balance[addr] -= e['value']
+					elif e['vout_height'] == block_height and e['vout_txid'] == txid:
+						alter_balance[addr] += e['value']
+					else:
+						raise Exception( 'unknown transaction_link' )
+
+				# レコードを追加する
+				for addr, gain in alter_balance.items():
+					# 直前までの残高と次のシリアルナンバーを取得する
+					balance = 0
+					serial = 0
+					c.execute( 'SELECT serial, balance FROM balance WHERE addresses = %s ORDER BY serial DESC LIMIT 1', (addr,) )
+					for e in c.fetchall():
+						balance = e['balance']
+						serial = e['serial'] + 1
+
+					# 残高更新
+					balance += gain
+
+					# レコード追加
+					c.execute(
+						'INSERT INTO balance VALUES ( %s, %s, %s, %s, %s, %s, %s )',
 						(
 							addr,
-							balance
-						)
-					)
-					db.close()
-					conn.commit()
-				else:
-					db = conn.cursor()
-					db.execute(
-						'UPDATE current_balance SET balance = %s WHERE addresses = %s',
-						(
+							block_height,
+							txid,
+							serial,
+							datetime.fromtimestamp( cd_transaction['time'] ),
 							balance,
-							addr
+							gain
 						)
 					)
-					db.close()
-					conn.commit()
 
-		# ブロックヘッダの作成
-		db = conn.cursor()
-		db.execute(
-			'INSERT INTO blockheader VALUES( %s, %s, %s, %s, %s )',
-			(
-				block_height,
-				cd_block['hash'],
-				datetime.fromtimestamp( cd_block['time'] ),
-				miners,
-				base64.b64encode( bz2.compress( json.dumps( block_json_reduce ).encode() ) )
+					# 最終残高の更新
+					if serial == 0:
+						c.execute(
+							'INSERT INTO current_balance VALUES ( %s, %s )',
+							(
+								addr,
+								balance
+							)
+						)
+					else:
+						c.execute(
+							'UPDATE current_balance SET balance = %s WHERE addresses = %s',
+							(
+								balance,
+								addr
+							)
+						)
+
+			# ブロックヘッダの作成
+			c.execute(
+				'INSERT INTO blockheader VALUES( %s, %s, %s, %s, %s )',
+				(
+					block_height,
+					cd_block['hash'],
+					datetime.fromtimestamp( cd_block['time'] ),
+					miners,
+					base64.b64encode( bz2.compress( json.dumps( block_json_reduce ).encode() ) )
+				)
 			)
-		)
-		db.close()
-		conn.commit()
 
 # 1 ブロック分データを巻き戻す
 # - 何度同じ height で実行しても問題ない
@@ -562,11 +541,12 @@ def run( coind_type, max_leng ):
 		sync( conn, coind_type, max_leng )
 
 	# 終了時のポイントを取得する
-	db.execute( 'SELECT IFNULL(MAX(height)+1,0) FROM blockheader' )
-	end_block_height = db.fetchone()['IFNULL(MAX(height)+1,0)']
+	with conn.cursor() as c:
+		c.execute( 'SELECT IFNULL(MAX(height)+1,0) FROM blockheader' )
+		end_block_height = c.fetchone()['IFNULL(MAX(height)+1,0)']
 
 	# ロック解除
-	with db as c:
+	with conn.cursor() as c:
 		c.execute( 'UPDATE state SET running_flag = 0, running_time = NULL' )
 
 	# 実行結果をログと戻り値の双方に記述する
@@ -593,38 +573,3 @@ class handler( BaseHandler ):
 		coind_type = self.get_request_coind_type(request)
 		max_leng = self.get_request_int( request, 'max_leng', 7000 )
 		return json.dumps( run( coind_type, max_leng ) )
-
-
-def test( coind_type, max_leng ):
-	logging.getLogger().setLevel( logging.DEBUG )
-
-	try:
-		# 指定された coind のデータベースへ接続する
-		conn = CloudSQL( coind_type )
-	except MySQLdb.OperationalError:
-		# 接続に失敗した場合、データベースの作成から行う
-		conn = init_db( coind_type )
-		logging.debug( coind_type + ': DB initialized.' )
-
-	# 同時実行を防ぐため、ロックをかける
-	with conn.cursor() as c:
-		# 同時実行がない場合、もしくは指定秒数以上経過していれば UPDATE に成功する
-		c.execute( 'UPDATE state SET running_flag = 1, running_time = NOW() WHERE running_flag = 0 OR TIMESTAMPADD( SECOND, %d, running_time ) < NOW()' % LOCK_TIMEOUT )
-		conn.commit()
-		if c.rowcount != 1:
-			# ロック確保に失敗したらここで止める
-			raise Exception( 'running another!!' )
-		
-	# 開始時のポイントを覚えておく
-	with conn.cursor() as c:
-		c.execute( 'SELECT IFNULL(MAX(height)+1,0) FROM blockheader' )
-		start_block_height = c.fetchone()['IFNULL(MAX(height)+1,0)']
-	
-	# ここで DB 更新作業を行う
-	if check_db_state( conn, coind_type ):
-		# 巻き戻しを行わなかった場合のみ更新に進む
-		# sync( conn, coind_type, max_leng )
-		print("check done")
-	
-	
-	return 1
